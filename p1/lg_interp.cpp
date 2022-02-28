3f9ddb7e8afdeba73d29b3bfeabf1553df331631
Israel Bonilla <ib0574@princeton.edu>
2022-02-28 11:31:23 -0500

    initialized helper functions for lg_interp

---
+++ p1/lg_interp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <fstream>
+#include <limits>
+
+int runge_exact(double* x, double* f);
+
+int abs_exact(double* x, double* f);
+
+int step_exact(double* x, double* f);
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
