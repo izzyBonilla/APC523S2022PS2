8af1939a227e46bf0ae50f26ed24dca056f56884
Israel Bonilla <ib0574@princeton.edu>
2022-03-10 16:51:24 -0500

    started adding plotting infrastructure for error part

--- p1/p1.sh
+++ p1/p1.sh
@@ -14,5 +14,23 @@ gnuplot -e "filename='p1_1b.png'" gnu.sh
 ./interp step
 gnuplot -e "filename='p1_1c.png'" gnu.sh 
 
+# runge err
+for ((i=10; i <= 1000; c+=500))
+do
+	ERR=$(./interp_err runge $i 0)
+	echo $ERR >> errsdat
+done
+
+make clean
+rm *.dat
+#!/bin/bash
+make poisson1c
+for NTHREAD in 1 2 4 8 16
+do
+	export OMP_NUM_THREADS=$NTHREAD
+	TIME=$(mpiexec -n poisson1c $NTHREAD 128 1e-6 | awk '/Finished in/ {print $3}')
+	echo $NTHREAD $TIME >> runtimes.dat
+done
+gnuplot p1b_gnu.sh
 make clean
 rm *.dat
