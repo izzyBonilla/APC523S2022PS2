9cc777a758532eeef3031c2046a476aff30b40fd
Israel Bonilla <ib0574@princeton.edu>
2022-03-14 11:17:51 -0400

    added infrastructure for looping on lg_err

--- p1/p1.sh
+++ p1/p1.sh
@@ -15,22 +15,10 @@ gnuplot -e "filename='p1_1b.png'" gnu.sh
 gnuplot -e "filename='p1_1c.png'" gnu.sh 
 
 # runge err
-for ((i=10; i <= 1000; c+=500))
+for ((i=10; i <= 1000; i+=50))
 do
-	ERR=$(./interp_err runge $i 0)
-	echo $ERR >> errsdat
+	./interp_err runge $i 0 >> testing
 done
 
-make clean
-rm *.dat
-#!/bin/bash
-make poisson1c
-for NTHREAD in 1 2 4 8 16
-do
-	export OMP_NUM_THREADS=$NTHREAD
-	TIME=$(mpiexec -n poisson1c $NTHREAD 128 1e-6 | awk '/Finished in/ {print $3}')
-	echo $NTHREAD $TIME >> runtimes.dat
-done
-gnuplot p1b_gnu.sh
 make clean
 rm *.dat
