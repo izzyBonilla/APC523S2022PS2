13b581ddad1d9d96efb9c1701e056fdd3e07eab7
Israel Bonilla <ib0574@princeton.edu>
2022-03-15 14:04:45 -0400

    fixed plot error

--- p2/gnu.sh
+++ p2/gnu.sh
@@ -7,8 +7,8 @@ set ylabel "f"
 set title "Error vs. h"
 
 plot "gq_a.dat" u 1:2 w lines tit "error",\
-plot x**0.5 dashtype 2 tit "h^0.5",\
-plot x dashtype 2 tit "h",\
-plot x**2 dashtype 2 tit "h^2",\
-plot x**4 dashtype 2 tit "h^4",\
-plot x**10 dashtype 2 tit "h^10"
+     x**0.5 dashtype 2 tit "h^0.5",\
+     x dashtype 2 tit "h",\
+     x**2 dashtype 2 tit "h^2",\
+     x**4 dashtype 2 tit "h^4",\
+     x**10 dashtype 2 tit "h^10"
