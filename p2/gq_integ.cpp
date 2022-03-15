1d1e178d7cc276eb68bc0608b754a0aabf9a4674
Israel Bonilla <ib0574@princeton.edu>
2022-03-15 13:28:35 -0400

    finished gaussian quadrature

--- p2/gq_integ.cpp
+++ p2/gq_integ.cpp
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <vector>
 #include <functional>
-#include <omp.h>
 
 #define P 5 // num points to avoid magic numbering
 
@@ -33,7 +32,7 @@ int main(int argc, char *argv[]) {
     }
     std::function <double(double)> f;
 
-    if(func == "x8") {
+    if(func == "x8") { // lambdas
         f = [](double x){return pow(x,8);};
     } else if(func == "abs_poly") {
         f = [](double x){return pow(abs(x-1./sqrt(2)),3);};
@@ -53,21 +52,25 @@ int main(int argc, char *argv[]) {
                               0.5688888888888889,0.4786286704993665,0.2369268850561891};
 
     double L = b-a; // require b > a, need L > 0
-    double h = L/n;
+    double h = L/(n*P);
+    double dh = L/n;
 
     double x0;
     double f0; 
 
-    double sum = 0;
+    double integ = 0;
 
-    for(i=1; i < n+1; ++i) {
+    for(i=0; i < n; ++i) {
         for(j=0; j < P; ++j) {
-                x0 = (i-1)*h + (xg[j]+1)/2*h;
-                f(x0,f0);
-                sum += h*f0*wg[j]/2;
+                x0 = dh/2*(xg[j]+1)+a+i*dh;
+                f0 = f(x0);
+                integ += wg[j]*f0;
         }
     }
 
-    double integ = sum;
+    integ *= dh/2;
+
+    std::cout << h << "\t" << integ << "\n";
+
     return 0;
 }
