c34963507305a76393a514307b74ec4d9ee0f87d
Israel Bonilla <ib0574@princeton.edu>
2022-03-15 13:47:36 -0400

    added looping in bash file

--- p2/p2.sh
+++ p2/p2.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 make gq
 
-# Part A
-for i in {1..10}
+# Part A x8
+INPUT=gq_a.dat
+for i in 1 2 4 8 16 32 64 128 256
 do
-    echo $i
-    ./gq $i -1 1 x8
+    ./gq $i -1 1 x8 >> INPUT
+done
+
+# Part B abs_poly
+INPUT=gq_b.dat
+for i in 1 2 4 8 16 32 64 128 256
+do
+    ./gq $i -1 1 x8 >> INPUT
+done
+
+# Part C step
+INPUT=gq_c.dat
+for i in 1 2 4 8 16 32 64 128 256
+do
+    ./gq $i -1 1 x8 >> INPUT
+done
+
+# Part D inv sqrt
+INPUT=gq_d.dat
+for i in 1 2 4 8 16 32 64 128 256
+do
+    ./gq $i -1 1 x8 >> INPUT
 done
