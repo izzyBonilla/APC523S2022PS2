f916482e30b464dbd802a0c25c4a74672234f548
Israel Bonilla <ib0574@princeton.edu>
2022-03-15 13:55:27 -0400

    added gnuplot script

---
+++ p2/gnu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/gnuplot
+
+set term png
+set output outfile
+set xlabel "h"
+set ylabel "f"
+set title "Error vs. h"
+
+plot infile u 1:2 w lines tit "error",\
+plot x**0.5 dashtype 2 tit "h^0.5",\
+plot x dashtype 2 tit "h",\
+plot x**2 dashtype 2 tit "h^2",\
+plot x**4 dashtype 2 tit "h^4",\
+plot x**10 dashtype 2 tit "h^10"
--- p2/p2.sh
+++ p2/p2.sh
@@ -3,13 +3,17 @@ make gq
 
 # Part A x8
 INPUT=gq_a.dat
+OUTPUT=p2_a.png
 for i in 1 2 4 8 16 32 64 128 256
 do
     ./gq $i -1 1 x8 >> $INPUT
 done
 
+gnuplot -e "infile=$INPUT,outfile=$OUTPUT" gnu.sh
+
 # Part B abs_poly
 INPUT=gq_b.dat
+OUTPUT=p2_b.png
 for i in 1 2 4 8 16 32 64 128 256
 do
     ./gq $i -1 1 x8 >> $INPUT
@@ -17,6 +21,7 @@ done
 
 # Part C step
 INPUT=gq_c.dat
+OUTPUT=p2_c.png
 for i in 1 2 4 8 16 32 64 128 256
 do
     ./gq $i -1 1 x8 >> $INPUT
@@ -24,6 +29,7 @@ done
 
 # Part D inv sqrt
 INPUT=gq_d.dat
+OUTPUT=p2_d.png
 for i in 1 2 4 8 16 32 64 128 256
 do
     ./gq $i -1 1 x8 >> $INPUT
